4f7e1aaf5f1d90dfdcbc1311c79db60e1c6861c9
Gary <garrypu445@gmail.com>
2024-02-09 20:15:26 +0800

    update

--- vue/src/main.js
+++ vue/src/main.js
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import VueI18n from 'vue-i18n'
 
 
 const app = createApp(App)
@@ -14,16 +13,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-const i18n = new VueI18n({
-    locale: 'zh_TW',
-    fallbackLocale: 'zh_TW',
-    messages,
-});
-
-// 3. WebSocket setup
-app.use(VueNativeSock, `${process.env.VUE_APP_WSS}`, {
-    connectManually: true,
-});
-
-// 7. Export i18n
-export default i18n;
+app.mount('#app')
